2ce64253c7bbb22e28982b2ff0cbc8ea31bcdc4a
Tushar Mehta <109963328+LUCIFERMORNINGSTAR-728@users.noreply.github.com>
2022-10-11 12:13:13 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-        <div className="n-name">Tushar</div>
+        <div className="n-name">Lucifer</div>
         <Toggle />
       </div>
       {/* right */}
