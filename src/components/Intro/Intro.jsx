733288fc333053f124350c1fc6e8b50cda9bd1cd
Tushar Mehta <109963328+LUCIFERMORNINGSTAR-728@users.noreply.github.com>
2022-10-11 12:14:07 +0530

    Update Intro.jsx

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -28,7 +28,7 @@ const Intro = () => {
         <div className="i-name">
           {/* yahan change hy darkmode ka */}
           <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
-          <span>Tushar Mehta</span>
+          <span>Lucifer Morningstar</span>
           <span>
             Frontend Developer with high level of experience in web designing
             and development, producting the Quality work
