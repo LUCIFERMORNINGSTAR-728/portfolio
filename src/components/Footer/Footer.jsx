815b44a1633ef0d840fa0de85d073a8babcf0e21
Tushar Mehta <109963328+LUCIFERMORNINGSTAR-728@users.noreply.github.com>
2022-10-11 12:14:49 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <div className="footer">
       <img src={Wave} alt="" style={{ width: "100%" }} />
       <div className="f-content">
-        <span>tusharmehta728@gmial.com</span>
+        <span>lucifermorningstar6007@gmial.com</span>
         <div className="f-icons">
           <Insta color="white" size={"3rem"} />
           <Facebook color="white" size={"3rem"} />
